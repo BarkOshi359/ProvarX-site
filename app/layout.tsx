38ca4a40a41bc6145449aa8b22b5c45c5f7e4dd4
BarkOshi359 <cwells1444@gmail.com>
2026-06-30 12:02:18 -0400

    insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,6 +39,31 @@ export default function RootLayout({
           data-pid="1tquFa1wC8YHwkcH9"
           data-version="062024"
         />
+        {/* LinkedIn Insight Tag */}
+        <Script id="linkedin-insight-init" strategy="afterInteractive">
+          {`_linkedin_partner_id = "9350946";
+window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+window._linkedin_data_partner_ids.push(_linkedin_partner_id);`}
+        </Script>
+        <Script id="linkedin-insight" strategy="afterInteractive">
+          {`(function(l) {
+if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+window.lintrk.q=[]}
+var s = document.getElementsByTagName("script")[0];
+var b = document.createElement("script");
+b.type = "text/javascript";b.async = true;
+b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+s.parentNode.insertBefore(b, s);})(window.lintrk);`}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            alt=""
+            src="https://px.ads.linkedin.com/collect/?pid=9350946&fmt=gif"
+          />
+        </noscript>
       </body>
     </html>
   );
